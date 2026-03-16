75ce418eefe8dd96989bb561b9101b57d42d10f9
ajuanijustus <ajuani1997@gmail.com>
2026-03-16 20:18:52 +0000

    Add time.sleep in worker init and rollback thread handling in main

--- multi_processing.py
+++ multi_processing.py
@@ -52,6 +52,7 @@ class MultiProcessTrainer(object):
             self.comms.append(comm)
             worker = MultiProcessWorker(i, trainer_maker, comm_remote, seed=args.seed)
             worker.start()
+            time.sleep(1)
             print("[Trainer] workers started")
         print("[Trainer] done creating workers")
         self.grads = None
